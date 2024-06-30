31c456003ff577a0be9d4ba9ec49d262afa91889
Myslitel666 <tretyakov_02@bk.ru>
2024-07-01 00:35:49 +0500

    Fixed a bug with selective font application.

--- src/components/common/user-interface/InputBase.svelte
+++ src/components/common/user-interface/InputBase.svelte
@@ -1,19 +1,21 @@
 <script>
     export let height = '3rem'                             /* Высота поля */
     export let width = '15rem'                             /* Ширина поля */
-    export let borderRadius = '4.5px';                     /* Радиус скругления углов */
-    export let transition = 'border-color 0.3s ease, outline-color 0.3s ease';      /* Тайм-код для применения того или иного эффекта */
-    export let paddingLeft = '0.6rem';                     /* Отступ от левой границы до курсора */
-    export let paddingRight = '0.6rem';                    /* Отступ до правой границы */
-    export let fontSize = '1.25rem';                       /* Размер шрифта */
-    export let primaryColor = '#12ce0e';                   /* Основной цвет*/
+    export let borderRadius = '0.28rem';                   /* Радиус скругления углов */
+    export let transition = 'border-color 0.3s ease';      /* Тайм-код для применения того или иного эффекта */
+    export let paddingLeft = '0.85rem';                    /* Отступ от левой границы до курсора */
+    export let paddingRight = '0.85rem';                   /* Отступ до правой границы */
+    export let fontSize = '1.12rem';                       /* Размер шрифта */
+    export let labelColor = '#c8c8c8';                     /* Цвет надписи по умолчанию*/
+    export let primaryColor = '#5bb056';                   /* Основной цвет*/
     export let borderColor = '#c4c4c4';                    /* Цвет обводки */
     export let cursor = 'text';                            /* Курсор */
     export let borderWidth = '1px';                        /* Толщина обводки */
     export let borderStyle = 'solid';                      /* Тип обводки */
-    export let hoverBorderColor = 'black';                 /* Цвет обводки при наведении*/
-    export let focusBorderWidth = '4px';                   /* Толщина обводки при нажатии*/
-
+    export let hoverBorderColor = 'black';                 /* Цвет обводки при наведении */
+    export let focusBorderWidth = '10px';                   /* Толщина обводки при нажатии */
+    export let labelFontSizeActive = '0.81rem';            /* Размер шрифта надписи при нажатии */
+    export let backgroundColor = 'white';                  /* Цвет фона */
 </script>
 
 <div 
@@ -33,6 +35,8 @@
 		style:border-width = {borderWidth}
         style:outline-color = {primaryColor}
         style:outline-width = {focusBorderWidth}
+        style:font-width = 0.5rem
+        style:--primaryColor = {primaryColor}
         style:--borderColor = {borderColor}
         style:--hoverBorderColor = {hoverBorderColor}
     />
@@ -40,24 +44,31 @@
         for='input-base'
         style:position = 'absolute'
         style:margin-left = {paddingLeft}
-        style:font-size = {fontSize}
-        style:color = '#c8c8c8'
+        style:--primaryColor = {primaryColor}
+        style:--font-size = {fontSize}
+        style:--color = {labelColor}
         style:--hover-cursor = {cursor}
+        style:--labelFontSizeActive = {labelFontSizeActive}
+        style:--labelBgColor = {backgroundColor}
     >
         Course
     </label>
 </div>
 
 <style>
+    input {
+        border-color: var(--borderColor);
+    }
+
+    label {
+        font-size: var(--font-size);
+        color: var(--color);
+    }
+
     .input-container {
         display: flex;
         justify-content: center;
         flex-direction: column;
-    }
-
-    input {
-        border-color: var(--borderColor);
-        cursor: var(--hover-cursor);
     }
 
     input:hover {
@@ -68,15 +79,11 @@
         cursor: var(--hover-cursor);
     }
 
-    .input-container input {
-    }
-
-    input::placeholder {
-        color:  var(--placeholder-color); 
-        transition: var(--placeholder-transition);
-    }
-
-    input:focus::placeholder {
-        color: transparent;
+    input:focus + label {
+        transform: translate(-0.26rem, -1.55rem); /* Сдвигаем метку влево и вверх */
+        font-size: var(--labelFontSizeActive); /* Уменьшаем размер шрифта */
+        color: var(--primaryColor); /* Изменяем цвет на основной цвет */
+        background-color: var(--labelBgColor);
+        padding: 0.26rem;
     }
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,6 @@
     import '../../Font.css';
 </script>
 
-<style>
-    .app {
-        font-family: 'Century Gothic', sans-serif;
-		font-weight: -400;
-		letter-spacing: -0.05rem;
-    }
-</style>
-
 <div class="app">
 	<main>
 		<slot />
