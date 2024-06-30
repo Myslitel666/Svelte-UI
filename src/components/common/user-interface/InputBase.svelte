9fa6b1192216bf3e34590cf2a8d86c017c708a59
Myslitel666 <tretyakov_02@bk.ru>
2024-07-01 02:00:22 +0500

    Added a check for non-emptiness of the InputBase component.

--- src/components/common/user-interface/InputBase.svelte
+++ src/components/common/user-interface/InputBase.svelte
@@ -1,8 +1,9 @@
 <script>
     export let height = '3rem'                             /* Высота поля */
     export let width = '15rem'                             /* Ширина поля */
+    export let label = 'Course'                            /* Надпись */
     export let borderRadius = '0.28rem';                   /* Радиус скругления углов */
-    export let transition = 'border-color 0.3s ease';      /* Тайм-код для применения того или иного эффекта */
+    export let effectsTimeCode = '0.3s';                   /* Тайм-код для применения того или иного эффекта */
     export let paddingLeft = '0.85rem';                    /* Отступ от левой границы до курсора */
     export let paddingRight = '0.85rem';                   /* Отступ до правой границы */
     export let fontSize = '1.12rem';                       /* Размер шрифта */
@@ -13,9 +14,11 @@
     export let borderWidth = '1px';                        /* Толщина обводки */
     export let borderStyle = 'solid';                      /* Тип обводки */
     export let hoverBorderColor = 'black';                 /* Цвет обводки при наведении */
-    export let focusBorderWidth = '10px';                   /* Толщина обводки при нажатии */
+    export let focusBorderWidth = '2px';                   /* Толщина обводки при нажатии */
     export let labelFontSizeActive = '0.81rem';            /* Размер шрифта надписи при нажатии */
     export let backgroundColor = 'white';                  /* Цвет фона */
+
+    let inputValue = ''; // Переменная для хранения значения ввода
 </script>
 
 <div 
@@ -25,10 +28,11 @@
     <input 
         type = 'text'
         id = 'input-base'
+        placeholder = ''
         style:height = {height}
         style:border-radius = {borderRadius}
         style:border-style = {borderStyle}
-        style:transition = {transition}
+        style:transition = 'border-color {effectsTimeCode} ease'
         style:padding-left = {paddingLeft}
         style:padding-right = {paddingRight}
         style:font-size = {fontSize}
@@ -44,6 +48,7 @@
         for='input-base'
         style:position = 'absolute'
         style:margin-left = {paddingLeft}
+        style:transition = 'all {effectsTimeCode} ease'
         style:--primaryColor = {primaryColor}
         style:--font-size = {fontSize}
         style:--color = {labelColor}
@@ -51,7 +56,7 @@
         style:--labelFontSizeActive = {labelFontSizeActive}
         style:--labelBgColor = {backgroundColor}
     >
-        Course
+        {label}
     </label>
 </div>
 
@@ -80,9 +85,12 @@
     }
 
     input:focus + label {
+        color: var(--primaryColor); /* Изменяем цвет на основной цвет */
+    }
+
+    input:focus + label, input:not(:placeholder-shown) + label {
         transform: translate(-0.26rem, -1.55rem); /* Сдвигаем метку влево и вверх */
         font-size: var(--labelFontSizeActive); /* Уменьшаем размер шрифта */
-        color: var(--primaryColor); /* Изменяем цвет на основной цвет */
         background-color: var(--labelBgColor);
         padding: 0.26rem;
     }
