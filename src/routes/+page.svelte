aa6d72c9da0667a67a5d982d2b6eee03adb2045c
Myslitel666 <tretyakov_02@bk.ru>
2024-08-17 08:23:19 +0500

    Added a possible example of the AutoComplete component.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,25 @@
 <div id = 'page'>
 	<div>
+		<p>Text Field</p>
 		<TextField />
 	</div>
 	<div class='component'>
+		<p>Button</p>
 		<Button>
 			CONTAINED
 		</Button>
 	</div>
 	<div class='component'>
+		<p>Switch</p>
 		<Switch />
 	</div>
 	<div class='component'>
+		<p>Color Theme Switch</p>
 		<ColorThemeSwitch />
+	</div>
+	<div class='component'>
+		<p>AutoComplete</p>
+		<AutoComplete options={['Apple', 'Orange', 'Banana', 'Grape', 'Mango']} />
 	</div>
 </div>
 
@@ -20,6 +28,7 @@
 	import Switch from '../components/inputs/switches/Switch.svelte';
 	import ColorThemeSwitch from '../components/inputs/switches/ColorThemeSwitch.svelte';
 	import Button from '../components/inputs/buttons/Button.svelte';
+	import AutoComplete from '../components/inputs/auto-complete/AutoComplete.svelte';
 </script>
 
 <style>
