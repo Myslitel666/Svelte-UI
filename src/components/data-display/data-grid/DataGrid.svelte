5b6bf9f53e362e6493de9adbb9d87a90c774c38c
Myslitel666 <tretyakov_02@bk.ru>
2024-09-05 19:15:06 +0500

    Styles for DataGrid borders have been written.

--- src/components/data-display/data-grid/DataGrid.svelte
+++ src/components/data-display/data-grid/DataGrid.svelte
@@ -1,12 +1,36 @@
 <div 
-    class = 'cell data-grid'
+    class = 'data-grid border'
     style:--Xl-border-color = {borderColor}
 >
-    <div class = 'cell row'>
+    <div class = 'row border'>
+        <div class = 'cell border'>
+        </div>
+        <div class = 'cell border'>
+        </div>
+        <div class = 'cell border'>
+        </div>
+        <div class = 'cell border'>
+        </div>
     </div>
-    <div class = 'cell row'>
+    <div class = 'row border'>
+        <div class = 'cell border'>
+        </div>
+        <div class = 'cell border'>
+        </div>
+        <div class = 'cell border'>
+        </div>
+        <div class = 'cell border'>
+        </div>
     </div>
-    <div class = 'cell row'>
+    <div class = 'row border'>
+        <div class = 'cell border'>
+        </div>
+        <div class = 'cell border'>
+        </div>
+        <div class = 'cell border'>
+        </div>
+        <div class = 'cell border'>
+        </div>
     </div>
 </div>
 
@@ -36,9 +60,10 @@
 </script>
 
 <style>
-    .cell {
+    .border {
         border: 1px solid; /* Толщина и цвет обводки таблицы */
         border-color: var(--Xl-border-color);
+        transition: border-color var(--Xl-effectsTimeCode), background-color var(--Xl-effectsTimeCode);
     }
 
     .data-grid {
@@ -47,9 +72,31 @@
     }
 
     .row {
-        height: 2.5rem;
+        height: 3rem;
         border-left: none;
         border-right: none;
         border-top: none;
+        display: flex; /* Горизонтальная ориентация для ячеек */
+        flex: 1; /* Ячейки растягиваются на всю ширину строки */
+    }
+
+    .cell {
+        border-top: none;
+        border-bottom: none;
+        border-left: none;
+        flex: 1; /* Ячейки растягиваются на всю ширину строки */
+        border-top: none;
+        border-bottom: none;
+        display: flex; /* Используем Flexbox для вертикального выравнивания */
+        align-items: center; /* Центрируем содержимое по вертикали */
+        justify-content: center; /* Центрируем содержимое по горизонтали */
+    }
+
+    .row:last-child {
+        border-bottom: none; /* Убираем нижнюю границу у последней строки */
+    }
+
+    .cell:last-child {
+        border: none;
     }
 </style>
