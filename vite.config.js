69746ca4d7299b705819eb366f6b3e642e273fad
Myslitel666 <tretyakov_02@bk.ru>
2024-09-06 23:41:07 +0500

    Added the ability to connect to the server from a mobile device.

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { sveltekit } from "@sveltejs/kit/vite";
 
 /** @type {import('vite').UserConfig} */
 export default {
-  plugins: [sveltekit()],
+    plugins: [sveltekit()],
+    server: {
+      host: '0.0.0.0'
+    }
 };
