30d28153df50d9de6ef787ddc88050f19f79bac5
Myslitel666 <tretyakov_02@bk.ru>
2024-09-14 21:27:42 +0500

    AutoComplete has become exportable.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,6 +5,7 @@ export { default as ColorThemeSwitch } from './elegant/customization/ColorThemeS
 export { default as Button } from './elegant/input/Button.svelte';
 export { default as Switch } from './elegant/input/Switch.svelte';
 export { default as TextField } from './elegant/input/TextField.svelte';
+export { default as AutoComplete } from './elegant/input/AutoComplete.svelte';
 export { default as DataGrid } from './elegant/data-display/DataGrid.svelte';
 export { default as Light } from './icons-elegant/Light.svelte';
 export { default as Dark } from './icons-elegant/Dark.svelte';
